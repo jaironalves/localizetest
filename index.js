b24a2a7af65dfded46a28e8cc006e387d7d83391
Jairon Alves Lima <jairon.lima@linx.com.br>
2020-07-13 09:56:29 -0300

    Using only deepDash

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const _ = require("lodash");
 //mixin all the methods into Lodash object
 require("deepdash")(_);
 
-const fs = require('fs');
+const fs = require("fs");
 
 const button = require("./button.json");
-let buttonLocalized = require("./button.json");
-const translations = require('./translations/Button-pt.json');
+//let buttonToLocalize = require("./button.json");
+const translations = require("./translations/Button-pt.json");
 
 let objectsL10n = [];
 
@@ -26,28 +26,67 @@ const localize = (key, fallBack) => {
   return localized;
 };
 
-_.eachDeep(button, (value, key, _parentValue, context) => {
-  if (_.startsWith(key, "muiL10n")) {
-    let omit = context._item.path;
-    let apply = convert(omit);
-    let key = _.camelCase(apply.join(" "));
-    objectsL10n.push({
-      key,
-      omit,
-      apply,
-      stringValue: value,
-      stringLocalized: localize(key, value)
-    });    
+_.eachDeep(
+  button,
+  (value, key, _parentValue, context) => {
+    if (_.startsWith(key, "muiL10n")) {
+      //console.log(context.path)
+      let omit = context.path;
+      let apply = convert(omit);
+      let key = _.camelCase(apply.join(" "));
+      objectsL10n.push({
+        key,
+        omit,
+        apply,
+        stringValue: value,
+        stringLocalized: localize(key, value),
+      });
+    }
+  },
+  { pathFormat: "array" }
+);
+
+// objectsL10n.forEach((objectL10n) => {
+//   _.set(buttonLocalized, objectL10n.apply, objectL10n.stringLocalized);
+//   _.set(buttonLocalized, objectL10n.omit, undefined);
+// });
+
+
+
+let buttonLocalizedValues = _.mapValuesDeep(
+  button,
+  (value, key, _parentValue, context) => {
+    if (_.startsWith(key, "muiL10n")) {
+      let omit = context.path;
+      let apply = convert(omit);
+      let keyFind = _.camelCase(apply.join(" "));
+      return localize(keyFind, value)
+    }
+
+    return value;
+  },
+  { pathFormat: "array" }
+);
+
+let buttonLocalized = _.mapKeysDeep(
+  buttonLocalizedValues,
+  (_value, key) => {
+    if (_.startsWith(key, "muiL10n")) {
+      let newKey = _.camelCase(key.replace("muiL10n", ""));      
+      return newKey;
+    }
+
+    return key;
+  },
+  { pathFormat: "array" }
+);
+
+fs.writeFile(
+  "Button.localized.json",
+  JSON.stringify(buttonLocalized, undefined, 2),
+  (err) => {
+    if (err) return console.log(err);
   }
-});
-
-objectsL10n.forEach(objectL10n => {
-  _.set(buttonLocalized, objectL10n.apply, objectL10n.stringLocalized);
-  _.set(buttonLocalized, objectL10n.omit, undefined);
-});
-
-fs.writeFile('Button.localized.json', JSON.stringify(buttonLocalized, undefined, true), (err) => {
-  if (err) return console.log(err);  
-});
+);
 
 console.log(objectsL10n);
